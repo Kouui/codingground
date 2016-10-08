89383a9ebae13e3be08493641a2bff1248a340ef
Kouui <lelouchkako@gmail.com>
2016-10-08 17:34:15 +0900

    Update do_while_loop.c

--- learning C programing/do_while_loop.c
+++ learning C programing/do_while_loop.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+/*
+to see the difference btween do_while loop and while loop
+*/
+int main()
+{
+  int i=11;
+  do
+  { 
+    printf("Value of i = %d \n",i); 
+    i++;
+  } while (i <= 10);
+  
+  int j=11;
+  while (j <= 10)
+  {
+    printf("Value of j = %d \n",j);
+    j++
+  }
+}
