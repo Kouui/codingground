c5c09157fc064e3acc9aacb9d5eb9cec2dc8e80d
Kouui <lelouchkako@gmail.com>
2016-10-08 19:03:57 +0900

    Created by Coding Ground Client

---
+++ learning C programing/for_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    for (int a=0; a<=1; a++)   
+    {
+        printf("Value of a = %d \n",a);
+    }
+}
+
+/*
+the result a=0 and a=1 says
+1. int a=0
+2. whether condition a<1 hold
+3. if it hold goto the content below
+4. a++
+5. goto 2.
+*/
+
