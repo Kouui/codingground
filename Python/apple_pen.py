f9b0cda8e7f93a05376791ed87f4eaa9f554bdd7
Kouui <lelouchkako@gmail.com>
2016-10-09 23:32:47 +0900

    Update apple_pen.py

--- Python/apple_pen.py
+++ Python/apple_pen.py
@@ -1,1 +1,12 @@
 
+import time
+
+str1='I have a pen'
+str2='I have an apple'
+str3='ee!'
+str4='Applepen'
+mystr=[str1,str2,str3,str4]
+
+for index in range(len(mystr)):
+       print (mystr[index])
+        time.sleep(3)  # Delay for 3 sec 
