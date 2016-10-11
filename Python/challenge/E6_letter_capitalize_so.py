b27f5ac56a7d04c236b0201031516ce1bfc72c26
Kouui <lelouchkako@gmail.com>
2016-10-11 17:58:02 +0900

    Update and rename letter_capitalize_so.py to E6_letter_capitalize_so.py

--- Python/challenge/E6_letter_capitalize_so.py
+++ Python/challenge/E6_letter_capitalize_so.py
@@ -7,7 +7,7 @@ def LetterCapitalize(str):
   # the first part is the first letter which we capitalize and the 
   # second part is the rest of the string
   for i in range(0, len(words)): 
-    words[i] = words[i][0].upper() + words[i][1:]
+    words[i] = words[i][0].upper() + words[i][1:]  # words[i] = words[i].capitalize()
 
   # return the list of words joined into a string
   return " ".join(words)
