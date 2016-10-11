b7dd995f86af924fa4e25821d70dca94af5ffb6e
Kouui <lelouchkako@gmail.com>
2016-10-11 16:50:53 +0900

    Create simple_adding.py

---
+++ Python/challenge/simple_adding.py
@@ -0,0 +1,8 @@
+def SimpleAdding(num): 
+
+    # code goes here
+    # sum(arg,start=i) returns the total sum of lst named arg from starting point arg[i]
+    return sum(range(num+1),start=1)
+    
+# keep this function call here  
+print SimpleAdding(raw_input())
